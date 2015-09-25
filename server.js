fba0708416bda6f33890a00063a5acbac628b6cc
Livvie Lin <livvie.lin@gmail.com>
2015-09-25 14:38:29 -0700

    Set up express server and dependencies

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+// Serve up client files
+app.use(express.static(__dirname + '/client'));
+app.use(bodyParser.json());
+
+// Set up port
+var port = 3000;
+// Have app listen on port specified above
+app.listen(port);
+console.log('Listening on port ' + port);
