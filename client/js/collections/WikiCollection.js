803963ff2137ae07b375ff7c5d506040cfc5bb78
Livvie Lin <livvie.lin@gmail.com>
2015-10-14 21:45:45 -0700

    Format search terms to capitalize first letter of words

--- client/js/collections/WikiCollection.js
+++ client/js/collections/WikiCollection.js
@@ -6,10 +6,17 @@ var WikiCollection = Backbone.Collection.extend({
   url: 'http://localhost:3000/wiki',
 
   addWiki: function (wiki) {
-    this.add({ 'searchTerm': wiki });
+    // Format input by capitalizing first letters
+    var showTitle = wiki;
+    var showTitleSplit = showTitle.split(' ');
+    for (var i = 0; i < showTitleSplit.length; i++) {
+      showTitleSplit[i] = showTitleSplit[i][0].toUpperCase() + showTitleSplit[i].substring(1);
+    }
+    var formattedTitle = showTitleSplit.join(' ');
+    this.add({ 'searchTerm': formattedTitle });
 
     // Instantiate a new wiki model to save to the database
-    var wikiModel = new WikiModel({ 'searchTerm': wiki });
+    var wikiModel = new WikiModel({ 'searchTerm': formattedTitle });
     // POST search term to database
     wikiModel.save(null, {
       success: function (response) {
